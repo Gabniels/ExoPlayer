406e47f74894bac3d6e166da0bcb7b749d1aa9b1
DANIL_UZAIRI <daniluzairi1@gmail.com>
2023-04-21 21:21:23 +0700

    .

--- app/src/main/java/com/gabniel/exoplayer/MainActivity.kt
+++ app/src/main/java/com/gabniel/exoplayer/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.gabniel.exoplayer
 
-import android.content.pm.ActivityInfo
+import android.R
+import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.gabniel.exoplayer.databinding.ActivityMainBinding
 import com.gabniel.exoplayer.exoplayer.ExoVideoPlayer
+import com.google.android.exoplayer2.SimpleExoPlayer
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,30 +20,39 @@ class MainActivity : AppCompatActivity() {
     private lateinit var exoVideoPlayer: ExoVideoPlayer
     private var list: ArrayList<String>? = null
 
+
+    private var mFullScreenDialog: Dialog? = null
+    private var mExoPlayerFullscreen = false
+    private val mExoPlayerView: SimpleExoPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+//        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+//        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         initializationExo()
         getData()
     }
 
     override fun onPause() {
         super.onPause()
+        Log.d("TAG-LIFECYCLE", "onPause: ")
         exoVideoPlayer.pauseExo()
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d("TAG-LIFECYCLE", "onResume: ")
         exoVideoPlayer.destroyExo()
         exoVideoPlayer.initializationExoPlayer(list)
         exoVideoPlayer.positionExo()
     }
 
     private fun initializationExo() {
-        exoVideoPlayer = ExoVideoPlayer(applicationContext, binding.exoVideo)
+        exoVideoPlayer = ExoVideoPlayer(applicationContext, binding.exoVideo, this)
     }
 
     private fun getData() {
--- app/src/main/java/com/gabniel/exoplayer/exoplayer/ExoVideoPlayer.kt
+++ app/src/main/java/com/gabniel/exoplayer/exoplayer/ExoVideoPlayer.kt
@@ -1,23 +1,48 @@
 package com.gabniel.exoplayer.exoplayer
 
+import android.app.Activity
 import android.content.Context
+import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.util.Log
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.ImageView
+import com.gabniel.exoplayer.R
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.PlaybackException
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ui.StyledPlayerView
+import com.google.android.exoplayer2.ui.StyledPlayerView.GONE
 import com.google.android.exoplayer2.ui.StyledPlayerView.SHOW_BUFFERING_WHEN_PLAYING
 
 class ExoVideoPlayer(
     var context: Context,
     var exoVideoLayout: StyledPlayerView,
+    var activity: Activity,
 ) {
 
     private var indexVideo = 0
     private var exoPlayer: ExoPlayer? = null
+
+    //    private var position: Long = 0
     private var position: Long = 0
+    private var btnPrevious: ImageView? = null
+    private var btnNext: ImageView? = null
+    private var btnFullScreen: ImageView? = null
+    private var btnMinimize: ImageView? = null
+    private var btnPlay: ImageView? = null
+    private var btnPause: ImageView? = null
+    private var fullScreen = true
+
+    init {
+        btnPlay = exoVideoLayout.findViewById(R.id.btnPlay)
+        btnPause = exoVideoLayout.findViewById(R.id.btnPause)
+        btnPrevious = exoVideoLayout.findViewById(R.id.btnPrevious)
+        btnNext = exoVideoLayout.findViewById(R.id.btnNext)
+    }
 
     fun initializationExoPlayer(data: ArrayList<String>?) {
         if (!data.isNullOrEmpty()) {
@@ -40,6 +65,9 @@ class ExoVideoPlayer(
                 override fun onPlaybackStateChanged(playbackState: Int) {
                     super.onPlaybackStateChanged(playbackState)
                     when (playbackState) {
+                        Player.STATE_BUFFERING -> {
+                            btnPlay?.visibility = GONE
+                        }
                         Player.STATE_ENDED -> {
                             destroyExo()
                             indexVideo++
@@ -61,19 +89,72 @@ class ExoVideoPlayer(
                 useController = true
                 setShowBuffering(SHOW_BUFFERING_WHEN_PLAYING)
             }
+            onClickListener(data)
         }
     }
+
+    private fun onClickListener(data: ArrayList<String>) {
+        btnPrevious?.setOnClickListener {
+            previousVideo(data)
+        }
+
+        btnPause?.setOnClickListener {
+            pauseVideo()
+        }
+        btnPlay?.setOnClickListener {
+            playVideo()
+        }
+
+        btnNext?.setOnClickListener {
+            nextVideo(data)
+        }
+    }
+
+    private fun pauseVideo() {
+        btnPause?.visibility = GONE
+        btnPlay?.visibility = VISIBLE
+        exoPlayer?.pause()
+    }
+
+    private fun playVideo() {
+        btnPause?.visibility = VISIBLE
+        btnPlay?.visibility = GONE
+        exoPlayer?.play()
+    }
+
+    private fun previousVideo(data: ArrayList<String>) {
+        destroyExo()
+        indexVideo--
+        if (indexVideo != 0) {
+            indexVideo = data.size - 1
+        }
+        initializationExoPlayer(data)
+    }
+
+    private fun nextVideo(data: ArrayList<String>) {
+        destroyExo()
+        indexVideo++
+        if (indexVideo >= data.size) {
+            indexVideo = 0
+        }
+        initializationExoPlayer(data)
+    }
+
 
     fun pauseExo() {
         destroyExo()
         if (exoPlayer != null) {
             position = exoPlayer!!.currentPosition
+            btnPlay?.visibility = View.VISIBLE
         }
     }
 
     fun positionExo() {
         if (exoPlayer != null) {
+            btnPause?.visibility = View.VISIBLE
             exoPlayer?.seekTo(position)
+            Log.d("TAG-ROTATE", "positionExo: $position")
+            exoPlayer?.playbackState
             exoPlayer?.playWhenReady = true
         }
     }
@@ -84,6 +165,7 @@ class ExoVideoPlayer(
             exoPlayer?.release()
         }
     }
+
 
     companion object {
         val TAG = ExoVideoPlayer::class.java.simpleName
